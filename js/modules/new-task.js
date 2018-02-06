3ef0fa2a0432e23d9241798a3494edc5d9f92485
Sylwuchaa <uzumagi1@gmail.com>
2018-02-06 22:27:33 +0100

    try to cofig gulp

--- js/modules/new-task.js
+++ js/modules/new-task.js
@@ -3,9 +3,9 @@ function addNewTasks(title) {
 
     taskLi.classList.add('single-task');
     taskLi.innerHTML = prepareTaskHTML(title);
-    // Add task to DOM 
+    // Add task to DOM
     tasksContainer.appendChild(taskLi);
-} 
+}
 
 function prepareTaskHTML(title) {
   return  '<div class="input-group">' +
@@ -13,11 +13,11 @@ function prepareTaskHTML(title) {
             '<button class="btn btn-light toggle-complete_btn">' +
             '<i class="fa fa-check"></i></button>' +
         '</span>' +
-    
+
     '<input type="text" class="form-control" placeholder="Tytył zadania..." value="Pójść do sklepu">' +
         '<span class="input-group-btn">' +
             '<button class="btn btn-danger delete-task-btn">' +
             '<i class="fa fa-times"></i></button>' +
         '</span>' +
     '</div>';
-}
+} 
